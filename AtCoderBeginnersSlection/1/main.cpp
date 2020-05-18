8e0d4a2284719d484871f24ccfe2aa66985295ca
haruka <smgsummers64@gmail.com>
2020-05-18 14:09:36 +0900

    ABS 1問目

---
+++ AtCoderBeginnersSlection/1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	string s;
+	cin >> a;
+	cin >> b >> c;
+	cin >> s;
+
+	std::cout << a + b + c << " " << s << std::endl;
+	return 0;
+}
