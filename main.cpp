f46a25f7c017e7bb2b0fe69e3dd79359ee9facf5
haruka <smgsummers64@gmail.com>
2020-05-19 16:21:06 +0900

    リアルタイムな入力取得を実装

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "GameLib/Framework.h"
 using namespace GameLib;
+using namespace std;
 
 #include "State.h"
 #include "File.h"
@@ -7,6 +8,10 @@ using namespace GameLib;
 void mainLoop();
 
 State* gState = 0;
+bool gPrevInputS = false;
+bool gPrevInputA = false;
+bool gPrevInputW = false;
+bool gPrevInputZ = false;
 
 namespace GameLib{
 	void Framework::update(){
@@ -38,7 +43,7 @@ void mainLoop(){
 		gState = new State( file.data(), file.size() );
 		//初回描画
 		gState->draw();
-		return; //そのまま終わる
+		return; 
 	}
 	bool cleared = false;
 	//メインループ
@@ -51,17 +56,38 @@ void mainLoop(){
 	}
 	//入力取得
 	cout << "a:left s:right w:up z:down. command?" << endl; //操作説明
-	char input;
-	cin >> input;
+	Framework f = Framework::instance();
+	int dx = 0;
+	int dy = 0;
+	bool inputA = f.isKeyOn('a');
+	bool inputS = f.isKeyOn('s');
+	bool inputW = f.isKeyOn('w');
+	bool inputZ = f.isKeyOn('z');
+	if (inputA && (!gPrevInputA)) {
+		dx -= 1;
+	}
+	else if (inputS && (!gPrevInputS)) {
+		dx += 1;
+	}
+	else if (inputW && (!gPrevInputW)) {
+		dy -= 1;
+	}
+	else if (inputZ && (!gPrevInputZ)) {
+		dy += 1;
+	}
+	gPrevInputA = inputA;
+	gPrevInputS = inputS;
+	gPrevInputW = inputW;
+	gPrevInputZ = inputZ;
 	//終了判定
-	if ( input == 'q' ){
+	if ( Framework::instance().isKeyOn('q')) {
 		delete gState;
 		gState = 0;
 		Framework::instance().requestEnd();
 		return;
 	}
 	//更新
-	gState->update( input );
+	gState->update(dx, dy);
 	//描画
 	gState->draw();
 }
