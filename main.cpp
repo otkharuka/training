da2057a3e9b311e775eddc044061f313c6fd73d9
haruka <smgsummers64@gmail.com>
2020-05-21 16:20:54 +0900

    タイトル画面の表示とゲーム本体への遷移を実装

--- main.cpp
+++ main.cpp
@@ -4,58 +4,95 @@ using namespace std;
 
 #include "State.h"
 #include "File.h"
+#include "Image.h"
 
 void mainLoop();
+void title();
+void game();
 
 State* gState = 0;
+Image* gTitleImage = 0;
 bool gPrevInputS = false;
 bool gPrevInputA = false;
 bool gPrevInputW = false;
 bool gPrevInputZ = false;
 
-namespace GameLib{
-	void Framework::update(){
+enum Sequence {
+	SEQUENCE_TITLE,
+	SEQUENCE_GAME,
+};
+
+Sequence gSec = SEQUENCE_TITLE;
+
+namespace GameLib {
+	void Framework::update() {
 		mainLoop();
 	}
 }
 
-void mainLoop(){
-	if ( Framework::instance().isEndRequested() ){
-		if ( gState ){
+void mainLoop() {
+	switch (gSec) {
+	case SEQUENCE_TITLE:
+		title();
+		break;
+	
+	case SEQUENCE_GAME:
+		game();
+		break;
+	}
+}
+
+void title() {
+	if (!gTitleImage) {//gIsTitleSequence=falseのとき初期化
+		gTitleImage = new Image("title.dds");
+	}
+	gTitleImage->draw(0, 0, 0, 0, gTitleImage->width(), gTitleImage->height());
+	//スペースを押したらゲーム画面に進む
+	if (Framework::instance().isKeyOn(' ')) {
+		gSec = SEQUENCE_GAME;
+		gTitleImage->init();
+		delete gTitleImage;
+	}
+}
+
+
+void game() {
+	if (Framework::instance().isEndRequested()) {
+		if (gState) {
 			delete gState;
 			gState = 0;
 		}
 		return;
 	}
 	//最初のフレーム	
-	if ( !gState ){ 
-		File file( "stageData.txt" );
+	if (!gState) {
+		File file("stageData.txt");
 
 		try {
-			if ( !( file.data() ) ){
+			if (!(file.data())) {
 				throw "stage file could not be read";
 			}
 		}
-		catch(char* str) {
+		catch (char* str) {
 			cout << str << endl;
 		}
 
-		gState = new State( file.data(), file.size() );
+		gState = new State(file.data(), file.size());
 		//初回描画
 		gState->draw();
-		return; 
+		return;
 	}
 	bool cleared = false;
 	//メインループ
 	//クリアチェック
-	if ( gState->hasCleared() ){
+	if (gState->hasCleared()) {
 		cout << "Congratulation! you win." << endl;
 		delete gState;
 		gState = 0;
+		//gIsTitleSequence = true;
 		return;
 	}
 	//入力取得
-	cout << "a:left s:right w:up z:down. command?" << endl; //操作説明
 	Framework f = Framework::instance();
 	int dx = 0;
 	int dy = 0;
@@ -80,7 +117,7 @@ void mainLoop(){
 	gPrevInputW = inputW;
 	gPrevInputZ = inputZ;
 	//終了判定
-	if ( Framework::instance().isKeyOn('q')) {
+	if (Framework::instance().isKeyOn('q')) {
 		delete gState;
 		gState = 0;
 		Framework::instance().requestEnd();
@@ -91,5 +128,3 @@ void mainLoop(){
 	//描画
 	gState->draw();
 }
-
-
