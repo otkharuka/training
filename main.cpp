cf24bd372ca9e3684d7ae40554f0a2ee8bbcea8e
haruka <smgsummers64@gmail.com>
2020-05-22 13:58:54 +0900

    安直にシーケンス遷移を実装

--- main.cpp
+++ main.cpp
@@ -8,10 +8,22 @@ using namespace std;
 
 void mainLoop();
 void title();
+void select();
+void load();
 void game();
+void menu();
+void clear();
 
 State* gState = 0;
 Image* gTitleImage = 0;
+Image* gSelectImage = 0;
+Image* gLoadImage = 0;
+Image* gMenuImage = 0;
+Image* gClearImage = 0;
+
+
+
+
 bool gPrevInputS = false;
 bool gPrevInputA = false;
 bool gPrevInputW = false;
@@ -19,10 +31,15 @@ bool gPrevInputZ = false;
 
 enum Sequence {
 	SEQUENCE_TITLE,
+	SEQUENCE_SELECT,
+	SEQUENCE_LOAD,
 	SEQUENCE_GAME,
+	SEQUENCE_MENU,
+	SEQUENCE_CLEAR,
 };
 
-Sequence gSec = SEQUENCE_TITLE;
+Sequence gSeq = SEQUENCE_TITLE;
+int gSelect;
 
 namespace GameLib {
 	void Framework::update() {
@@ -31,28 +48,113 @@ namespace GameLib {
 }
 
 void mainLoop() {
-	switch (gSec) {
+	switch (gSeq) {
 	case SEQUENCE_TITLE:
 		title();
 		break;
-	
+	case SEQUENCE_SELECT:
+		select();
+		break;
+	case SEQUENCE_LOAD:
+		load();
+		break;
 	case SEQUENCE_GAME:
 		game();
+		break;
+	case SEQUENCE_MENU:
+		menu();
+		break;
+	case SEQUENCE_CLEAR:
+		clear();
 		break;
 	}
 }
 
+void drawImage(Image* image, const char* filename) {
+	if (!image) {
+		image = new Image(filename);
+	}
+	image->draw(0, 0, 0, 0, image->width(), image->height());
+	
+}
+
 void title() {
-	if (!gTitleImage) {//gIsTitleSequence=falseのとき初期化
-		gTitleImage = new Image("title.dds");
-	}
-	gTitleImage->draw(0, 0, 0, 0, gTitleImage->width(), gTitleImage->height());
-	//スペースを押したらゲーム画面に進む
+	drawImage(gTitleImage, "imageData/title.dds");
+	
+	//スペースを押したらセレクト画面に進む
 	if (Framework::instance().isKeyOn(' ')) {
-		gSec = SEQUENCE_GAME;
+		gSeq = SEQUENCE_SELECT;
 		gTitleImage->init();
-		delete gTitleImage;
 	}
+	delete gTitleImage;
+}
+
+
+void select() {
+	drawImage(gSelectImage, "imageData/stageSelect.dds");
+	
+	//１～９を押してステージを選ぶ
+	if (Framework::instance().isKeyOn('1')) {
+		gSelect = 1;
+		gSeq = SEQUENCE_LOAD;
+		gSelectImage->init();
+	}
+	delete gSelectImage;
+}
+
+
+void load() {
+	drawImage(gLoadImage, "imageData/loading.dds");
+	gSeq = SEQUENCE_GAME;
+	gLoadImage->init();
+	delete gLoadImage;
+}
+
+
+void menu() {
+	drawImage(gMenuImage, "imageData/menuAlpha.dds");
+	Framework f = Framework::instance();
+	//やりなおし
+	if (f.isKeyOn('1')) {
+		//ゲームを初期化
+		delete gState;
+		gState = 0;
+
+		gLoadImage->init();
+		delete gMenuImage;
+		gSeq = SEQUENCE_GAME;
+	}
+	//ステージ選択
+	else if (f.isKeyOn('2')) {
+		delete gState;
+		gState = 0;
+
+		gLoadImage->init();
+		delete gMenuImage;
+		gSeq = SEQUENCE_SELECT;
+	}
+	//タイトル
+	else if (f.isKeyOn('3')) {
+		delete gState;
+		gState = 0;
+
+		gLoadImage->init();
+		delete gMenuImage;
+		gSeq = SEQUENCE_TITLE;
+	}
+	//ゲームに戻る
+	else if (f.isKeyOn('4')) {
+		gLoadImage->init();
+		delete gMenuImage;
+		gSeq = SEQUENCE_GAME;
+	}
+}
+
+void clear() {
+	drawImage(gClearImage, "imageData/clear.dds");
+	gSeq = SEQUENCE_TITLE;
+	gLoadImage->init();
+	delete gClearImage;
 }
 
 
@@ -82,14 +184,19 @@ void game() {
 		gState->draw();
 		return;
 	}
-	bool cleared = false;
+	
 	//メインループ
 	//クリアチェック
 	if (gState->hasCleared()) {
 		cout << "Congratulation! you win." << endl;
 		delete gState;
 		gState = 0;
-		//gIsTitleSequence = true;
+		gSeq = SEQUENCE_CLEAR;
+		return;
+	}
+	//menuボタンがおされていないかチェック
+	if (Framework::instance().isKeyOn('m')) {
+		gSeq = SEQUENCE_MENU;
 		return;
 	}
 	//入力取得
