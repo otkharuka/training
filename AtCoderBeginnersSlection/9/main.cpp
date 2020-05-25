2a32240e6afc5936257ba33ded4a17540a26bb40
haruka <smgsummers64@gmail.com>
2020-05-25 12:04:39 +0900

    ABS 9問目

---
+++ AtCoderBeginnersSlection/9/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N, Y;
+	cin >> N >> Y;
+	
+	for (int x = 0; x <= N; x++) {
+		for (int y = 0; y <= N; y++) {
+			int z = N - (x + y);
+			if (z >= 0) {
+				if (10000 * x + 5000 * y + 1000 * z == Y) {
+					cout << x << ' ' << y << ' ' << z << endl;
+					cin >> z;
+					return 0;
+				}
+			}
+			
+		}
+	}
+	cout << -1 << ' ' << -1 << ' ' << -1 << endl;
+	return 0;
+
+}
